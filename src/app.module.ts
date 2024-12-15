68865938e60758bc1a69fa7370186889be2ec2a9
__coma__ <codeliner96@gmail.com>
2024-12-15 20:19:30 +0900

    feat: Implemented health check module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { VideoModule } from './video/video.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [VideoModule],
+  imports: [VideoModule, HealthModule],
   controllers: [],
   providers: [],
 })
