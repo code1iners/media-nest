008d9e7cde6bdae6cb9ac3ac32ec4a278e442821
__coma__ <codeliner96@gmail.com>
2025-01-01 23:58:47 +0900

    feat: Added CORS configs

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,24 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const port = process.env.PORT || 3030;
 
+  const allowedOrigins = [
+    'http://localhost:5959',
+    'https://codia-api.codeliners.cc',
+  ];
+
   const app = await NestFactory.create(AppModule);
+
+  // Add CORS configs.
+  app.enableCors({
+    origin: (origin, callback) => {
+      console.log(origin);
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'), false);
+      }
+    },
+  });
   await app.listen(port);
 }
 bootstrap();
