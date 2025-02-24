c9794266f87bff5c2089b353ebc03b4a0c15c8c9
__coma__ <codeliner96@gmail.com>
2025-02-24 23:18:13 +0900

    fix: Added chrome(or other browsers) extension id environment variable

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const port = process.env.PORT || 3030;
+  console.log();
 
   const allowedOrigins = [
     'http://localhost:5959',
-    'chrome-extension://ioeimgdkibkanjcldfbbpmpabiibdkom',
-    'extension://ioeimgdkibkanjcldfbbpmpabiibdkom',
+    `chrome-extension://${process.env.EXTENSION_ID}`,
+    `extension://${process.env.EXTENSION_ID}`,
   ];
 
   const app = await NestFactory.create(AppModule);
@@ -15,10 +16,10 @@ async function bootstrap() {
   // Add CORS configs.
   app.enableCors({
     origin: (origin, callback) => {
-      console.log(origin);
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
+        console.log('origin ', origin);
         callback(new Error('Not allowed by CORS'), false);
       }
     },
