58e8519228f5512a72c9e65d92bb27ee4aedab49
__coma__ <codeliner96@gmail.com>
2025-02-24 23:42:34 +0900

    debug: Added debug log

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const port = process.env.PORT || 3030;
-  console.log();
+  const logger = new Logger('Main');
 
-  const allowedOrigins = [
-    'http://localhost:5959',
-    `chrome-extension://${process.env.EXTENSION_ID}`,
-    `extension://${process.env.EXTENSION_ID}`,
-  ];
+  const port = process.env.PORT || 3030;
+
+  const allowedOrigins = ['http://localhost:5959'];
+
+  if (process.env.EXTENSION_ID) {
+    allowedOrigins.push(`chrome-extension://${process.env.EXTENSION_ID}`);
+    allowedOrigins.push(`extension://${process.env.EXTENSION_ID}`);
+  }
+
+  logger.log(`EXTENSION_ID = ${process.env.EXTENSION_ID}`);
 
   const app = await NestFactory.create(AppModule);
 
